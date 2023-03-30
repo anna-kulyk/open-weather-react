cdb129ecbf6c892d9fe9bbe7a1e81d78aef9baa2
Anna <kulyk_anna@outlook.com>
2023-03-30 13:38:29 -0400

    initial commit

---
+++ src/Components/Weather/Weather.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import './Weather.css';
+
+
+const Weather = () => {
+
+    const API_KEY = 'your_api_key_here';
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Weather;
