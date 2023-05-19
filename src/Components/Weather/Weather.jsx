d4e7f5408219195f21280fec9ccd6c9003a67af3
Anna <kulyk_anna@outlook.com>
2023-05-19 15:04:39 -0400

    move functions-helpers to the separate file

--- src/Components/Weather/Weather.jsx
+++ src/Components/Weather/Weather.jsx
@@ -3,6 +3,7 @@ import LocationInput from '../LocationInput/LocationInput';
 import './Weather.css';
 import Loader from '../Loader/Loader';
 import ToggleSwitch from '../ToggleSwitch/ToggleSwitch';
+import { convertTime, convertTimeDay } from '../../helpers/converters';
 
 
 const Weather = () => {
@@ -124,24 +125,3 @@ const Weather = () => {
 };
 
 export default Weather;
-
-function convertTime(unixTime, timezone) {
-    let timezoneOffset = new Date().getTimezoneOffset() * 60;
-    let date = new Date((unixTime + timezoneOffset + timezone) * 1000);
-    let hours = date.getHours();
-    let minutes = "0" + date.getMinutes();
-    let time = hours + ":" + minutes.slice(-2);
-    return time;
-}
-
-function convertTimeDay(unixTime, timezone) {
-    let timezoneOffset = new Date().getTimezoneOffset() * 60;
-    let date = new Date((unixTime + timezoneOffset + timezone) * 1000);
-    let hours = date.getHours();
-    let minutes = "0" + date.getMinutes();
-    const time = hours + ":" + minutes.slice(-2);
-    const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    const day = date.getDay();
-    let output = `${dayNames[day]} ${time}`;
-    return output;
-}
---
+++ src/helpers/converters.js
@@ -0,0 +1,22 @@
+function convertTime(unixTime, timezone) {
+    let timezoneOffset = new Date().getTimezoneOffset() * 60;
+    let date = new Date((unixTime + timezoneOffset + timezone) * 1000);
+    let hours = date.getHours();
+    let minutes = "0" + date.getMinutes();
+    let time = hours + ":" + minutes.slice(-2);
+    return time;
+}
+
+function convertTimeDay(unixTime, timezone) {
+    let timezoneOffset = new Date().getTimezoneOffset() * 60;
+    let date = new Date((unixTime + timezoneOffset + timezone) * 1000);
+    let hours = date.getHours();
+    let minutes = "0" + date.getMinutes();
+    const time = hours + ":" + minutes.slice(-2);
+    const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const day = date.getDay();
+    let output = `${dayNames[day]} ${time}`;
+    return output;
+}
+
+export { convertTime, convertTimeDay };
