4bfc9f068ab19a76f8fa01043f6545221195a062
Anna <kulyk_anna@outlook.com>
2023-05-23 13:15:57 -0400

    add axios

--- src/Components/ErrorMessage/ErrorMessage.jsx
+++ src/Components/ErrorMessage/ErrorMessage.jsx
@@ -1,17 +1,17 @@
 import './ErrorMessage.css';
 import PropTypes from 'prop-types';
 
-const ErrorMessage = ({ location }) => {
+const ErrorMessage = ({ errorMessage }) => {
     return (
         <>
             <div className="error-title">Ooops...</div>
-            <div className="error-message">{`Can't find this location: ${location}`}</div>
+            <div className="error-message">{errorMessage}</div>
         </>
     );
 };
 
 ErrorMessage.propTypes = {
-    location: PropTypes.string.isRequired
+    errorMessage: PropTypes.string.isRequired
 }
 
 export default ErrorMessage;
--- src/Components/Weather/Weather.jsx
+++ src/Components/Weather/Weather.jsx
@@ -8,6 +8,7 @@ import ErrorMessage from '../ErrorMessage/ErrorMessage';
 import WeatherCurrent from '../WeatherCurrent/WeatherCurrent';
 import WeatherDetails from '../WeatherDetails/WeatherDetails';
 import WeatherForecast from '../WeatherForecast/WeatherForecast';
+import axios from 'axios';
 
 
 const Weather = () => {
@@ -15,10 +16,12 @@ const Weather = () => {
     const API_KEY = 'bf35cac91880cb98375230fb443a116f';
 
     const [isLoading, setIsLoading] = useState(true);
+    const [isFailed, setIsFailed] = useState(false);
     const [weatherData, setWeatherData] = useState(null);
     const [forecastData, setForecastData] = useState(null);
     const [location, setLocation] = useState('Kyiv');
     const [units, setUnits] = useState(unitsData['imperial']);
+    const [errorMessage, setErrorMessage] = useState('');
     // console.log(weatherData);
 
     // if (window.navigator.geolocation) {
@@ -27,28 +30,38 @@ const Weather = () => {
     // }
 
     useEffect(() => {
-        setIsLoading(true)
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=${units.name}`)
-            .then((response) => response.json())
-            .then((data) => setWeatherData(data))
-            .catch((error) => console.log(error))
-        fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${API_KEY}&units=${units.name}`)
-            .then((response) => response.json())
-            .then((data) => setForecastData(data))
-            .catch((error) => console.log(error))
+        setIsFailed(false);
+        setIsLoading(true);
+
+        axios.all([
+            axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=${units.name}`),
+            axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${API_KEY}&units=${units.name}`)
+        ])
+            .then(axios.spread((weather, forecast) => {
+                setWeatherData(weather.data);
+                setForecastData(forecast.data)
+            }))
+            .catch((error) => {
+                setIsFailed(true);
+                if (error.response && error.response.status === 404) {
+                    setErrorMessage(`Can't find this location: ${location}`);
+                } else {
+                    setErrorMessage(error.message);
+                }
+            })
             .finally(() => setIsLoading(false));
     }, [location, units]);
 
-    if (!weatherData || !forecastData) return (
+    if (!isFailed && (!weatherData || !forecastData)) return (
         <Loader />
     );
 
-    if (weatherData.cod === '404' || forecastData.cod === '404') {
+    if (isFailed) {
         return (
             <div className="weather">
                 <div className="weather-main">
                     <LocationInput newLocationHandler={setLocation} />
-                    <ErrorMessage location={location} />
+                    <ErrorMessage errorMessage={errorMessage} />
                 </div>
             </div>
         );
