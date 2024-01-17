699a5e238bbe0af70aa0832d7f0e59542db445c4
Anna <kulyk_anna@outlook.com>
2024-01-16 19:55:45 -0500

    refactor dateFormat

--- src/utils/dateFormat.js
+++ src/utils/dateFormat.js
@@ -1,6 +1,11 @@
-function formatTime(unixTime, timezone) {
+function convertUnixTimeToDate(unixTime, timezone) {
     let timezoneOffset = new Date().getTimezoneOffset() * 60;
     let date = new Date((unixTime + timezoneOffset + timezone) * 1000);
+    return date;
+}
+
+function formatTime(unixTime, timezone) {
+    let date = convertUnixTimeToDate(unixTime, timezone);
     let hours = date.getHours();
     let minutes = "0" + date.getMinutes();
     let time = hours + ":" + minutes.slice(-2);
@@ -8,13 +13,10 @@ function formatTime(unixTime, timezone) {
 }
 
 function formatTimeDay(unixTime, timezone) {
-    let timezoneOffset = new Date().getTimezoneOffset() * 60;
-    let date = new Date((unixTime + timezoneOffset + timezone) * 1000);
-    let hours = date.getHours();
-    let minutes = "0" + date.getMinutes();
-    const time = hours + ":" + minutes.slice(-2);
+    let date = convertUnixTimeToDate(unixTime, timezone);
+    let time = formatTime(unixTime, timezone);
     const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    const day = date.getDay();
+    let day = date.getDay();
     let output = `${dayNames[day]} ${time}`;
     return output;
 }
